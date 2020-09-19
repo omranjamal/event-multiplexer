962c4f71f495b44e2d8fabb93fbbea15fd84a116
thephoenixofthevoid <49817252+thephoenixofthevoid@users.noreply.github.com>
2020-09-19 09:46:01 +0300

    Reimplement the core
    Use and store at most 1 listener for each event name.

--- src/index.js
+++ src/index.js
@@ -3,94 +3,60 @@ import { EventEmitter } from 'events';
 export const OBJECT_ADDED = Symbol('OBJECT_ADDED');
 export const OBJECT_REMOVED = Symbol('OBJECT_REMOVED');
 
+function handleRemoveListener(ev) {
+    if (ev === "removeListener" || ev === "newListener") return;
+
+    // 'removeListener' is emitted after the listener is removed.
+    if (this.listenerCount(ev) == 0) {
+        const l = this._listeners.get(ev);
+        for (let obj of this._objects) obj.off(ev, l);
+        this._listeners.delete(ev)
+    }
+}
+
+function onNewListener(ev) { 
+    if (ev === "removeListener"||ev === "newListener") return;
+
+    // 'newListener' emitted before a listener is added to internal array
+    if (this.listenerCount(ev) == 0) {
+        const l = createListener(this, ev)
+        for (let obj of this._objects) obj.on(ev, l);
+        this._listeners.set(ev, l)
+    }
+}
+
+function createListener(mpx, event) {
+    return function listener(...args) {
+        mpx.emit(event, this, ...args)
+    }
+}
+
 export class EventMultiplexer extends EventEmitter {
-    _listening_on = new Map();
-    _listener_map = new Map();
-    _pass_objs = true;
 
-    constructor(pass_objs = true) {
+    _objects = new Set();
+    _listeners = new Map();
+
+    constructor() {
         super();
-
-        this._pass_objs = pass_objs;
-
-        this.on('removeListener', (ev_name, listener) => {
-            const listener_set = this._listening_on.get(ev_name);
-            listener_set.delete(listener);
-
-            if (listener_set.size === 0) {
-                for (const obj of this._listener_map.keys()) {
-                    obj.off(ev_name, this._listener_map.get(obj).get(ev_name));
-                    this._listener_map.get(obj).delete(ev_name);
-                }
-
-                this._listening_on.delete(ev_name);
-            }
-        });
-
-        this.on('newListener', (ev_name, listener) => {
-            if (ev_name === 'removeListener' || ev_name === 'newListener') {
-                return;
-            }
-
-            if (!this._listening_on.has(ev_name)) {
-                this._listening_on.set(ev_name, new Set());
-
-                for (const obj of this._listener_map.keys()) {
-                    let listener = null;
-
-                    if (this._pass_objs) {
-                        listener = (...args) => {
-                            this.emit(ev_name, obj, ...args);
-                        };
-                    } else {
-                        listener = (...args) => {
-                            this.emit(ev_name, obj, ...args);
-                        };
-                    }
-
-                    obj.on(ev_name, listener);
-                    this._listener_map.get(obj).set(ev_name, listener);
-                }
-            }
-
-            this._listening_on.get(ev_name).add(listener);
-        });
+        this.on('removeListener', handleRemoveListener);
+        this.on('newListener',    onNewListener);
     }
 
-    add(...objs) {
-        objs.forEach(obj => {
-            if (!this._listener_map.has(obj)) {
-                const ev_map = new Map();
-                this._listener_map.set(obj, ev_map);
-
-                for (const ev_name of this._listening_on.keys()) {
-                    const listener = (...args) => {
-                        this.emit(ev_name, obj, ...args);
-                    };
-
-                    obj.on(ev_name, listener);
-                    ev_map.set(ev_name, listener);
-                }
-
-                this.emit(OBJECT_ADDED, obj);
-            }
-        });
+    add(obj) {
+        if (this._objects.has(obj)) return;
+        this._objects.add(obj);
+        this._listeners.forEach((fn, ev) => obj.on(ev, fn))
+        this.emit(OBJECT_ADDED, obj);
     }
+    
+    remove(obj) {
+        if (!this._objects.has(obj)) return;
+        this._objects.delete(obj);
+        this._listeners.forEach((fn, ev) => obj.off(ev, fn))
+        this.emit(OBJECT_REMOVED, obj);
+    }   
 
-    remove(...objs) {
-        objs.forEach(obj => {
-            if (!this._listener_map.has(obj)) {
-                return;
-            }
-
-            const ev_map = this._listener_map.get(obj);
-
-            for (const ev_name of ev_map.keys()) {
-                obj.off(ev_name, ev_map.get(ev_name));
-            }
-
-            this._listener_map.delete(obj);
-            this.emit(OBJECT_REMOVED, obj);
-        });
-    }
+    // For tests
+    addMany   (...objs) { objs.map(this.add.bind(this))    }
+    removeMany(...objs) { objs.map(this.remove.bind(this)) }
 }
