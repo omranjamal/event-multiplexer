6d3c4d2d2045590e1264c26600ebb3eb1855e650
thephoenixofthevoid <49817252+thephoenixofthevoid@users.noreply.github.com>
2020-10-01 07:14:45 +0300

    Added .has and .values methods
    Added .has and .values methods. These are from Set<Emitter> interface. 
    EventMultiplexer actually implements both EventEmitter and Set interfaces for a reason: 
    It's a repository of objects, which also handles their events in a consistent way.
    
    Also, `values` method must be defined, because there should be a way to deal with objects
    in it, references to which have been lost by user. At least it allows to clear the Multiplexer.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import { EventEmitter } from 'events';
 
-export const OBJECT_ADDED = Symbol('OBJECT_ADDED');
+export const OBJECT_ADDED   = Symbol('OBJECT_ADDED');
 export const OBJECT_REMOVED = Symbol('OBJECT_REMOVED');
 
 function handleRemoveListener(ev) {
     if (ev === "removeListener" || ev === "newListener") return;
-
-    // 'removeListener' is emitted after the listener is removed.
     if (this.listenerCount(ev) == 0) {
         const l = this._listeners.get(ev);
         for (let obj of this._objects) obj.off(ev, l);
@@ -16,8 +14,6 @@ function handleRemoveListener(ev) {
 
 function onNewListener(ev) { 
     if (ev === "removeListener"||ev === "newListener") return;
-
-    // 'newListener' emitted before a listener is added to internal array
     if (this.listenerCount(ev) == 0) {
         const l = createListener(this, ev)
         for (let obj of this._objects) obj.on(ev, l);
@@ -42,21 +38,28 @@ export class EventMultiplexer extends EventEmitter {
         this.on('newListener',    onNewListener);
     }
 
+    has(obj) {
+        return this._objects.has(obj)
+    }
+
+    values() {
+        return this._objects.values()
+    }
+
     add(obj) {
-        if (this._objects.has(obj)) return;
+        if (this.has(obj)) return;
         this._objects.add(obj);
         this._listeners.forEach((fn, ev) => obj.on(ev, fn))
         this.emit(OBJECT_ADDED, obj);
     }
     
     remove(obj) {
-        if (!this._objects.has(obj)) return;
+        if (!this.has(obj)) return;
         this._objects.delete(obj);
         this._listeners.forEach((fn, ev) => obj.off(ev, fn))
         this.emit(OBJECT_REMOVED, obj);
-    }   
+    }
 
-    // For tests
     addMany   (...objs) { objs.map(this.add.bind(this))    }
     removeMany(...objs) { objs.map(this.remove.bind(this)) }
 }
